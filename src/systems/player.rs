5a1801199ce7a2212ace9656d2bd8606216d8374
Yanik Lyth <luckystreak63@protonmail.com>
2019-12-14 16:09:09 -0500

    Add jumping as an on-ground only event

--- src/systems/player.rs
+++ src/systems/player.rs
@@ -6,7 +6,7 @@ use amethyst::ecs::{
 use amethyst::input::{InputHandler, StringBindings};
 
 // You'll have to mark TILE_HEIGHT as public in pong.rs
-use crate::components::physics::{PlatformCuboid, Position, Velocity};
+use crate::components::physics::{Grounded, PlatformCuboid, Position, Velocity};
 use crate::components::player::Player;
 use crate::pizzatopia::{CAM_HEIGHT, TILE_HEIGHT};
 
@@ -18,15 +18,33 @@ impl<'s> System<'s> for PlayerInputSystem {
         WriteStorage<'s, Velocity>,
         Read<'s, InputHandler<StringBindings>>,
         ReadStorage<'s, Player>,
+        ReadStorage<'s, Grounded>,
     );
 
-    fn run(&mut self, (mut velocities, input, players): Self::SystemData) {
-        for (velocity, player) in (&mut velocities, &players).join() {
+    fn run(&mut self, (mut velocities, input, players, grounded): Self::SystemData) {
+        for (velocity, player, ground) in (&mut velocities, &players, (&grounded).maybe()).join() {
+            // Controller input
             let v_move = input.axis_value("vertical_move");
             let h_move = input.axis_value("horizontal_move");
+
+            // Get the grounded status to use auto-complete :)
+            let ground: Option<&Grounded> = ground;
+            let default_ground = Grounded(false);
+            let on_ground = ground.unwrap_or(&default_ground);
+            let on_ground = on_ground.0;
+
+            // Do the move logic
             if let Some(mv_amount) = v_move {
-                let scaled_amount = 0.3 * mv_amount as f32;
-                velocity.0.y += scaled_amount;
+                if mv_amount > 0.0 {
+                    if on_ground {
+                        let jump_velocity = 6.0;
+                        velocity.0.y += jump_velocity;
+                    }
+                }
+                // letting go of `up` will stop your jump
+                else if velocity.0.y > 0.0 {
+                    velocity.0.y /= 2.0;
+                }
             }
             if let Some(mv_amount) = h_move {
                 let scaled_amount = 0.1 * mv_amount as f32;
