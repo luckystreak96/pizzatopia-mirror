9a031b9fda600391976d90dbd9fd415a16544664
Yanik Lyth <luckystreak63@protonmail.com>
2020-08-01 00:10:26 -0400

    Add working tests for collisions

--- pizzatopia_physics/src/collider.rs
+++ pizzatopia_physics/src/collider.rs
@@ -1,9 +1,10 @@
 use amethyst_core::ecs::{Component, DenseVecStorage, Entity};
+use amethyst_core::num::Zero;
 use derivative::Derivative;
+use noisy_float::prelude::*;
 use rstar::{RTreeObject, AABB};
 use std::ops::{Add, Mul};
 use ultraviolet::Vec2;
-use noisy_float::prelude::*;
 
 /// When inserted as a `Resource` into `World`,
 /// affects the base strength of gravity on `RigidBody` components.
@@ -14,7 +15,7 @@ pub struct Gravity {
     pub strength: f32,
 }
 
-#[derive(Derivative, Component)]
+#[derive(Derivative, Component, Clone)]
 #[derivative(Default)]
 pub struct RigidBody {
     pub velocity: Vec2,
@@ -40,6 +41,30 @@ impl RigidBody {
                 true => current_collider.upper.x <= col.lower.x,
                 false => current_collider.lower.x >= col.upper.x,
             })
+            .filter(|col| {
+                projected.y.is_zero()
+                    || !projected.x.is_zero()
+                        && match projected.x > 0.0 {
+                            true => {
+                                let percent_distance = Vec2::new(
+                                    (col.lower.x - current_collider.upper.x) / projected.x,
+                                    (col.lower.y - current_collider.upper.y) / projected.y,
+                                );
+                                percent_distance.x > percent_distance.y
+                                    && percent_distance.x <= 1.0
+                                    && percent_distance.x > 0.0
+                            }
+                            false => {
+                                let percent_distance = Vec2::new(
+                                    (col.upper.x - current_collider.lower.x) / projected.x,
+                                    (col.upper.y - current_collider.lower.y) / projected.y,
+                                );
+                                percent_distance.x > percent_distance.y
+                                    && percent_distance.x <= 1.0
+                                    && percent_distance.x > 0.0
+                            }
+                        }
+            })
             .min_by_key(|col| match projected.x > 0.0 {
                 true => n32(col.lower.x),
                 false => n32(-col.upper.x),
@@ -51,6 +76,30 @@ impl RigidBody {
                 true => current_collider.upper.y <= col.lower.y,
                 false => current_collider.lower.y >= col.upper.y,
             })
+            .filter(|col| {
+                projected.x.is_zero()
+                    || !projected.y.is_zero()
+                        && match projected.y > 0.0 {
+                            true => {
+                                let percent_distance = Vec2::new(
+                                    (col.lower.x - current_collider.upper.x) / projected.x,
+                                    (col.lower.y - current_collider.upper.y) / projected.y,
+                                );
+                                percent_distance.x < percent_distance.y
+                                    && percent_distance.y <= 1.0
+                                    && percent_distance.y > 0.0
+                            }
+                            false => {
+                                let percent_distance = Vec2::new(
+                                    (col.upper.x - current_collider.lower.x) / projected.x,
+                                    (col.upper.y - current_collider.lower.y) / projected.y,
+                                );
+                                percent_distance.x < percent_distance.y
+                                    && percent_distance.y <= 1.0
+                                    && percent_distance.y > 0.0
+                            }
+                        }
+            })
             .min_by_key(|col| match projected.y > 0.0 {
                 true => n32(col.lower.y),
                 false => n32(-col.upper.y),
@@ -59,15 +108,15 @@ impl RigidBody {
         let mut result = (None, None);
         if let Some(col) = horizontal {
             let x = match projected.x > 0.0 {
-                true => col.lower.x - current_collider.upper.x,
-                false => col.upper.x - current_collider.lower.x,
+                true => col.lower.x - current_collider.upper.x - 0.0001,
+                false => col.upper.x - current_collider.lower.x + 0.0001,
             };
             result.0 = Some((x / time_scale, col.entity));
         }
         if let Some(col) = vertical {
             let y = match projected.y > 0.0 {
-                true => col.lower.y - current_collider.upper.y,
-                false => col.upper.y - current_collider.lower.y,
+                true => col.lower.y - current_collider.upper.y - 0.0001,
+                false => col.upper.y - current_collider.lower.y + 0.0001,
             };
             result.1 = Some((y / time_scale, col.entity));
         }
@@ -160,8 +209,8 @@ impl RTreeCollider {
         RTreeCollider {
             entity,
             opaque: collider.opaque,
-            lower: collider.lower,
-            upper: collider.upper,
+            lower: collider.lower.add(collider.position),
+            upper: collider.upper.add(collider.position),
         }
     }
 }
@@ -178,4 +227,84 @@ impl RTreeObject for RTreeCollider {
 pub struct ChildTo {
     pub parent: Entity,
     pub offset: Vec2,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::collider::{Collider, RTreeCollider, RigidBody};
+    use amethyst_core::ecs::world::Generation;
+    use amethyst_core::ecs::{Builder, Entity, WorldExt};
+    use amethyst_core::shred::World;
+    use rstar::RTree;
+    use std::num::NonZeroI32;
+    use ultraviolet::Vec2;
+
+    pub trait EqualsEps {
+        fn eq_eps(self, other: Self) -> bool;
+    }
+
+    impl EqualsEps for f32 {
+        fn eq_eps(self, other: Self) -> bool {
+            (self - other).abs() <= 0.01
+        }
+    }
+
+    #[test]
+    fn collision_velocity() {
+        let move_collider = Collider {
+            position: Vec2::default(),
+            lower: Vec2::new(0., 0.),
+            upper: Vec2::new(1.0, 1.0),
+            opaque: false,
+        };
+        let mut move_body = RigidBody {
+            velocity: Vec2::new(1.75, 1.75),
+            gravity: Vec2::default(),
+        };
+
+        let obstacle_1 = Collider {
+            position: Vec2::new(1.5, 1.0),
+            lower: Vec2::new(0., 0.),
+            upper: Vec2::new(1.0, 1.0),
+            opaque: true,
+        };
+        let obstacle_2 = Collider {
+            position: Vec2::new(0.75, 2.0),
+            lower: Vec2::new(0., 0.),
+            upper: Vec2::new(1.0, 1.0),
+            opaque: true,
+        };
+
+        let mut world = World::new();
+        world.register::<Collider>();
+        world.register::<RigidBody>();
+        let moving_entity = world
+            .create_entity()
+            .with(move_collider.clone())
+            .with(move_body.clone())
+            .build();
+        let obstacle1 = world.create_entity().with(obstacle_1.clone()).build();
+        let obstacle2 = world.create_entity().with(obstacle_2.clone()).build();
+        let rcollider = RTreeCollider::from_current_pos(moving_entity, &move_collider);
+        let non_refs = vec![
+            RTreeCollider::from_current_pos(obstacle1, &obstacle_1.clone()),
+            RTreeCollider::from_current_pos(obstacle2, &obstacle_2.clone()),
+        ];
+        let obstacles: Vec<&RTreeCollider> = non_refs.iter().collect();
+        let obstacles2 = obstacles.clone();
+        move_body.collide_with_nearest_axis(
+            &RTreeCollider::from_current_pos(moving_entity, &move_collider),
+            obstacles,
+            1.0,
+        );
+        assert!(move_body.velocity.x.eq_eps(0.5));
+        assert!(move_body.velocity.y.eq_eps(1.75));
+        move_body.collide_with_nearest_axis(
+            &RTreeCollider::from_current_pos(moving_entity, &move_collider),
+            obstacles2,
+            1.0,
+        );
+        assert!(move_body.velocity.x.eq_eps(0.5));
+        assert!(move_body.velocity.y.eq_eps(1.0));
+    }
 }
